635dc05f5a63447cb3c55ad5edb462ebd64e2522
aRN934 <61988380+aRN934@users.noreply.github.com>
2021-03-22 19:17:27 +0000

    update
    update para start, retirei também o primeiro if

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,44 +8,19 @@ public class Ball : MonoBehaviour
     [SerializeField]
     float velocidade = 5f;
 
-    float paradinha = 1f;
     bool bolaLancada = false;
-    
 
+  
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        //QUERO DIZER
-        //se >=2 segundos passaram verdadeiro - GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.right;
-
-
-        if (Time.deltaTime <= 2f) bolaLancada = false;
-         {
-            //QUERO DIZER
-            //A BOLA NÃO MEXE - LOL NÃO SEI ESCREVER ISTO
-
-
-
-            GetComponent<Rigidbody2D>().velocity = paradinha * Random.insideUnitSphere;
-
-         }
-        //AAAACHAVA QUE FAZIA SENTIDO MAS AFINAL A BOLA NÃO MEXE DE TODO
-        if (bolaLancada == true)
-        {
-            //*Ana se te der a burrice, já escreveste no onenote a lógica disto
-
-            //if (Random.value < 0.5f)
-            //{
-            //    GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.right;
-            //}
-            //else
-            //{
-            //    GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.left;
-            //}
+   
+        if (Time.deltaTime >= 2f) bolaLancada = true;
+        
 
             GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitSphere;
-        }
+        
 
     }
 }
